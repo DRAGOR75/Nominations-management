ff0921b24b24035b0b78cec9ccd9d2f37f68c034
Baibhav Gorai <63392805+DRAGOR75@users.noreply.github.com>
2026-01-07 12:55:06 +0530

    changed the migration issue

--- prisma/migrations/20260107061656_add_foundational_category/migration.sql
+++ prisma/migrations/20260107061656_add_foundational_category/migration.sql
@@ -2,4 +2,4 @@
 ALTER TYPE "TrainingCategory" ADD VALUE 'FOUNDATIONAL';
 
 -- AlterTable
-ALTER TABLE "training_sessions" ADD COLUMN     "send_feedback_automatically" BOOLEAN NOT NULL DEFAULT false;
+-- ALTER TABLE "training_sessions" ADD COLUMN     "send_feedback_automatically" BOOLEAN NOT NULL DEFAULT false;
